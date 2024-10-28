3aa62206f52bfde022f745a529667e8af21f6bf3
Iris Diaz Feijoo <irisdf_4@hotmail.com>
2024-10-28 13:53:23 +0100

    Terminado ejercicio 25

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,17 +4,28 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
-
+        Stack<String> stack = new Stack<>();
+        stack.push("Smith");
+        stack.push("Montessori");
+        stack.push("Peralta");
+        stack.push("House");
+        return stack;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
+        while (!stack.isEmpty()) {
+            String peekedItem = stack.peek();
+            System.out.println("Peeked Stack: " + peekedItem);
 
+            String removedItem = stack.pop();
+            System.out.println("Removed stack: " + removedItem);
+
+
+        }
     }
 
     public static void main(String[] args) {
-
+        Stack<String> myStack = createStack();
+        printAndEmptyStack(myStack);
     }
-
-
-
 }
